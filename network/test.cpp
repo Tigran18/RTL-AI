0052f52493bb3adf5fd9e09addc0c061cce83d0c
Tigran18 <tigrandavtyan18@gmail.com>
2025-04-27 20:27:18 +0400

    Network class implemented.

--- network/test.cpp
+++ network/test.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
 #include "network.hpp"
 
-int main(){
-    network m_network(3, {10, 15, 8});
-    std::cout<<m_network.get_layers()<<std::endl;
-    m_network.get_neurons();
+int main() {
+    std::vector<std::vector<double>> inputs = {
+        {0, 0},
+        {0, 1},
+        {1, 0},
+        {1, 1}
+    };
+
+    std::vector<std::vector<double>> targets = {
+        {0},
+        {1},
+        {1},
+        {1}
+    };
+
+    network net(3, {2, 3, 1}, 0.9, 4000); 
+
+    net.train(inputs, targets);
+
+    std::cout << "\nTesting network after training:\n";
+    for (size_t i = 0; i < inputs.size(); ++i) {
+        net.forward_propagate(inputs[i]);
+        std::cout << "Input (" << inputs[i][0] << ", " << inputs[i][1] << ") -> ";
+        net.display_outputs();
+    }
+
     return 0;
 }
