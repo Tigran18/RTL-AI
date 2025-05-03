c1da489b3c71825fe7b3f8adb6e13f7b5991699b
Tigran18 <tigrandavtyan18@gmail.com>
2025-05-03 12:30:39 +0400

    Binding all files.

---
+++ bindings.cpp
@@ -0,0 +1,21 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "/home/tigranda1809/RTL AI/network/network.hpp"
+#include "/home/tigranda1809/RTL AI/json_parser/json_parser.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(ai_module, m) {
+    py::class_<network>(m, "Network")
+        .def(py::init<std::vector<size_t>, double, size_t>())
+        .def("train", &network::train)
+        .def("predict", &network::predict)
+        .def("save_model", &network::save_model)
+        .def("load_model", &network::load_model)
+        .def("display_outputs", &network::display_outputs)
+        .def("forward_propagate", &network::forward_propagate);
+    py::class_<JSON>(m, "JSON")
+        .def(py::init<const std::string&, bool>())
+        .def("print", &JSON::print);
+}
+
---
+++ build/CMakeFiles/ai_module.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_module.dir/bindings.cpp.o"
+  "CMakeFiles/ai_module.dir/bindings.cpp.o.d"
+  "ai_module.cpython-312-x86_64-linux-gnu.so"
+  "ai_module.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
