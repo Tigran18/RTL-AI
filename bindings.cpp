4dc48c9bcccb14151d6256b215861fa3b6e49006
Tigran18 <tigrandavtyan18@gmail.com>
2025-05-10 17:52:13 +0400

    Some changes made.

--- bindings.cpp
+++ bindings.cpp
@@ -10,11 +10,12 @@ PYBIND11_MODULE(ai_module, m) {
         .def(py::init<std::vector<size_t>, double, size_t>())
         .def("train", &network::train)
         .def("predict", &network::predict)
-        .def("save_model", &network::save_model)
-        .def("load_model", &network::load_model)
+        .def("save_model", &network::save_model) 
+        .def("load_model", &network::load_model) 
         .def("display_outputs", &network::display_outputs);
-    py::class_<JSON>(m, "JSON")
-        .def(py::init<const std::string&>())
-        .def("print", &JSON::print);
-}
 
+    py::class_<JSON>(m, "JSON")
+        .def(py::init<const std::string&, bool>(), py::arg("input"), py::arg("isRawString") = true)
+        .def("print", &JSON::print)
+        .def("print_rtl", &JSON::print_rtl);
+}
